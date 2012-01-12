89c89d74b92c08c89c0c3fdba9c14aef6dfc2cdf
mauclair <tomaszpobozy@gmail.com>
2012-01-12 16:53:29 +0100

    changes for $default_prepended

--- classes/url.php
+++ classes/url.php
@@ -14,20 +14,18 @@ class URL extends Kohana_URL {
 	 * @param   mixed   language key to prepend to the URI, or FALSE to not prepend a language
 	 * @return  string
 	 */
-	public static function site($uri = '', $protocol = FALSE, $lang = TRUE)
-	{
-		if ($lang === TRUE)
-		{
-			// Prepend the current language to the URI
-			$uri = Request::$lang.'/'.ltrim($uri, '/');
-		}
-		elseif (is_string($lang))
-		{
-			// Prepend a custom language to the URI
-			$uri = $lang.'/'.ltrim($uri, '/');
-		}
+	public static function site($uri = '', $protocol = NULL, $index = TRUE, $lang = TRUE) {
+        	// Prepend language to URI if it needs to be prepended or it's not the default
+        	if ($lang === TRUE AND (Lang::$default_prepended OR Request::$lang !== Lang::$default)) {
+            		// Prepend the current language to the URI
+            		$uri = Request::$lang.'/'.ltrim($uri, '/');
+        	}
+        	elseif (is_string($lang)) {
+            		// Prepend a custom language to the URI
+            		$uri = $lang.'/'.ltrim($uri, '/');
+        	}
 
-		return parent::site($uri, $protocol);
-	}
+        	return parent::site($uri, $protocol, $index);
+    	}
 
 }
