ee5561c94dae4dbc82b9a244de16d7e4cb973923
mauclair <tomaszpobozy@gmail.com>
2012-01-12 16:55:28 +0100

    desc update

--- classes/url.php
+++ classes/url.php
@@ -2,18 +2,24 @@
 
 class URL extends Kohana_URL {
 
+	
+	
 	/**
-	 * Extension of URL::site that adds a third parameter for setting a language key.
-	 * The current language (Request::$lang) is used by default.
-	 *
-	 *     echo URL::site('foo/bar', FALSE, 'fr');  // custom language
-	 *     echo URL::site('foo/bar', FALSE, FALSE); // no language
-	 *
-	 * @param   string  site URI to convert
-	 * @param   mixed   protocol string or boolean, add protocol and domain?
-	 * @param   mixed   language key to prepend to the URI, or FALSE to not prepend a language
-	 * @return  string
-	 */
+     	* Extension of URL::site that adds a third parameter for setting a language key.
+     	* The current language (Request::$lang) is used by default.
+     	*
+	* echo URL::site('foo/bar', FALSE, TRUE, 'fr'); // custom language
+	* echo URL::site('foo/bar', FALSE, TRUE, FALSE); // no language
+	*
+	* @param string $uri Site URI to convert
+	* @param mixed $protocol Protocol string or [Request] class to use protocol from
+	* @param boolean $index Include the index_page in the URL
+	* @param mixed $lang Language key to prepend to the URI, or FALSE to not prepend a language
+	* @return string
+	* @uses Lang::$default_prepended
+	* @uses Request::$lang
+	*/
+	 
 	public static function site($uri = '', $protocol = NULL, $index = TRUE, $lang = TRUE) {
         	// Prepend language to URI if it needs to be prepended or it's not the default
         	if ($lang === TRUE AND (Lang::$default_prepended OR Request::$lang !== Lang::$default)) {
